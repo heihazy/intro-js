1138421c3414fa630963990d129ae764b5e5042d
Phuong Laitinen <55355626+heihazy@users.noreply.github.com>
2020-01-13 13:19:45 +0200

    Added test javascript

---
+++ js/script.js
@@ -0,0 +1,34 @@
+console.log('That was it');
+var a = 1;
+var b = 2;
+console.log(a+b);
+
+var a = 1;
+var b = 2;
+if (a>b) {
+    console.log('a is greater than b');
+}
+
+/*var a = 1;
+var b = 2;
+if (a<b) {
+    // do following things if a is lesser than b
+    console.log('a is lesser than b');
+    console.log(a + ' is lesser than ' + b);
+} else {
+    /* do following things otherwise 
+    console.log('a is not greater than b');
+}*/
+
+var a = 3;
+var b = 2;
+if(a<b) {
+    console.log('a is lesser than b');
+} 
+
+if ( a > b) { 
+    console.log('a is greater than b');
+}
+if(a === 1) {
+    console.log('a is exactly 1');
+}
