fe9e8f05db2ab60dd034c95eca39b7d12235efae
Phuong Laitinen <55355626+heihazy@users.noreply.github.com>
2020-01-27 14:33:16 +0200

    finished tasks using classList

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,54 @@
-(function () {
-    var itemClassName = 'slider__photo';
-        items = document.getElementsByClassName(itemClassName),
-        totalItems = items.length,
-        slide = 0;
+(function() {
+  var itemClassName = "slider__photo";
+  (items = document.getElementsByClassName(itemClassName)),
+    (totalItems = items.length),
+    (slide = 0);
 
-    // Set event listeners
-    function setEventListeners() {
-        var next = document.getElementsByClassName('slider__button--next')[0],
-            prev = document.getElementsByClassName('slider__button--prev')[0];
+  // Set event listeners
+  function setEventListeners() {
+    var next = document.getElementsByClassName("slider__button--next")[0],
+      prev = document.getElementsByClassName("slider__button--prev")[0];
 
-        next.addEventListener('click', getNext);
-        prev.addEventListener('click', getPrev);
-    }
+    next.addEventListener("click", getNext);
+    prev.addEventListener("click", getPrev);
+  }
 
-    function getNext() {
-        // TODO
-    }
+  function getNext() {
+    // TODO
+    //if (items[slide].classList.contains("active")) {
+    //items[slide].classList.remove("active");
+    //}
+    //slide++;
+    //items[slide].classList.add("active");
+    items[slide].classList.remove("active");
+    if (slide === totalItems - 1) {
+      slide = 0;
+      //slide = slide === totalItems - 1 ? 0 : slide++;
+    } else slide++;
+    items[slide].classList.add("active");
+  }
 
-    function getPrev(){
-        // TODO      
-    }
+  function getPrev() {
+    // TODO
+    //if (items[slide].classList.contains("active")) {
+    //items[slide].classList.remove("active");
+    //}
+    //slide--;
+    //items[slide].classList.add("active");
+    items[slide].classList.remove("active");
+    if (slide === 0) {
+      slide = totalItems - 1;
+    } else slide--;
+    items[slide].classList.add("active");
+  }
 
-    function initSlider() {
-        // setInitialClasses();
-        setEventListeners();
-        
-        // Set moving to false so that the slider becomes interactive
-        moving = false;
-    }
+  function initSlider() {
+    // setInitialClasses();
+    setEventListeners();
 
-    initSlider();
+    // Set moving to false so that the slider becomes interactive
+    moving = false;
+  }
+
+  initSlider();
 })();
