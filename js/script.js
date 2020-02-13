d07c4676bd45b8574778039530710e4065e05213
Phuong Laitinen <55355626+heihazy@users.noreply.github.com>
2020-02-13 14:49:57 +0200

    wrote javascript
    make the clock work and create select dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,32 @@
-var HOURHAND = document.getElementsById('hour');
-var MINUTEHAND = document.querySelector('.minute');
-var SECONDHAND = document.getElementsByName('second');
+var hourHand = document.querySelector("#hour");
+var minuteHand = document.querySelector("#minute");
+var secondHand = document.querySelector("#second");
+
+function runTheClock() {
+  // fetch current time
+  var now = new Date();
+  hr = now.getHours();
+  minutes = now.getMinutes();
+  sec = now.getSeconds();
+  //angle move per second
+  deltaSec = (sec * 360) / 60;
+  deltaMin = (minutes * 360) / 60 + deltaSec / 60;
+  deltaHr = (hr * 360) / 12 + (minutes * 360) / 60 / 12;
+  //style hands to current time
+  hourHand.style.transform = "rotate(" + deltaHr + "deg)";
+  minuteHand.style.transform = "rotate(" + deltaMin + "deg)";
+  secondHand.style.transform = "rotate(" + deltaSec + "deg)";
+}
+runTheClock();
+var interval = setInterval(runTheClock, 1000);
+
+var audio = new Audio(
+  "https://actions.google.com/sounds/v1/alarms/alarm_clock.ogg"
+);
+audio.play();
+
+var selectHour = document.getElementById("hours");
+var option = document.createElement("option");
+for (var i = 0; i < 24; i++) {
+  selectHour.options[selectHour.options.length] = new Option(i + 1, i);
+}
